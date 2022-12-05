0e8be330bdfce442cbbf4ec9d54a7be3e3395a63
Clint Jordan <clintaddison@gmail.com>
2022-12-04 21:36:27 -0700

    good, but slow

--- gen_link_table.sh
+++ gen_link_table.sh
@@ -9,10 +9,8 @@ user='cjordan'
 homeDir='/home/'$user
 ignoreFile='.link_table.ignore'
 ignoreRegex=$(cat $homeDir/configs/$HOSTNAME/$ignoreFile)
-
 fns=$(find $homeDir/configs/ -type f | grep -Ev "$ignoreRegex")
 fns=( $fns )
-
 maxChar=0
 
 # Find maximum number of characters in filenames
@@ -23,11 +21,15 @@ for fn in ${fns[@]}; do
     fi
 done
 
-# Print link table to $homeDir/configs/link_table
->| $homeDir/configs/$HOSTNAME/link_table
-maxChar=$(( $maxChar+1 ))
+# Print link table to std out
+maxChar=$(( $maxChar+3 ))
 for fn in ${fns[@]}; do
     inum=$(ls -i $fn | awk '{print $1}')
+    charCount=$(echo $fn | wc -L)
+    n=$(( maxChar-charCount ))
     str=$(find $homeDir /etc  -inum $inum | grep -v $homeDir'/configs')
-    printf "%-${maxChar}s %s   %s\n" $fn '-->' $str 
+    printf "%s " $fn 
+    printf "%0.s-" $(seq 1 $n)
+    printf "%s" '> ' 
+    printf "%s\n" $str 
 done
