b225788c4f4875aa990b50cffcee9c01177a9240
Clint Jordan <clintaddison@gmail.com>
2022-12-11 19:10:58 -0700

    added display manager conf

--- work/relink.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-
-ln -f ~/configs/bashrc ~/.bashrc
-ln -f ~/configs/bash_logout ~/.bash_logout
-ln -f ~/configs/bash_profile  ~/.bash_profile
-ln -f ~/configs/inputrc  ~/.inputrc
-ln -f ~/configs/tmux/tmux2.9a.conf  ~/.tmux.conf
-ln -f ~/configs/vimrc  ~/.vimrc
