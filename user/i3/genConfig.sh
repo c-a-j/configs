17a78aa1434674a4783fefd0421a04a7111d6050
Clint Jordan <clintaddison@gmail.com>
2025-11-07 12:25:19 -0500

    i3 robustness

--- user/i3/genConfig.sh
+++ user/i3/genConfig.sh
@@ -3,21 +3,59 @@
 set -eou pipefail
 
 configDir=$(dirname $0)
-i3src=$configDir/i3.conf
+i3template=$configDir/i3.config.template
+i3src=$configDir/i3.config
 i3dest=$HOME/.config/i3/config
 
-host=mbp
-wsFile=$configDir/$host/workspaces
-if [ $HOSTNAME == $host ]; then
-  sed -n "/{{WORKSPACE_SETUP}}/!{p;d;}; r $wsFile" $i3src |\
-  sed 's|{{SET_MOD_KEY}}|set \$mod mod4|g' > $i3dest
+layoutDir=$configDir/layouts
+
+colorSchemeFile="default"
+colorSchemeDir=$configDir/color-schemes
+colorScheme=$colorSchemeDir/$colorSchemeFile
+
+# ensure color scheme exists and is valid
+if [ ! -f $colorScheme ]; then
+  echo "error: color scheme file, $colorScheme, doesn't exist"
+  exit 1
+fi
+if ! i3 -c $colorScheme -C > /dev/null; then
+  echo "error: color scheme file, $colorScheme, is invalid"
+  exit 1
 fi
 
-host=nuc
-wsFile=$configDir/$host/workspaces
-if [ $HOSTNAME == $host ]; then
-  sed -n "/{{WORKSPACE_SETUP}}/!{p;d;}; r $wsFile" $i3src |\
-  sed 's|{{SET_MOD_KEY}}|set \$mod mod1|g' > $i3dest
+# check for host specific startup layout
+if [ -f $layoutDir/$HOSTNAME/startup ]; then
+  startupLayout=$layoutDir/$HOSTNAME/startup
+else
+  startupLayout=$layoutDir/default/startup
 fi
 
+case $HOSTNAME in
+  nuc)
+    modKey="mod1"
+    ;;
+  mbp)
+    modKey="mod4"
+    ;;
+  *)
+    modKey="mod1"
+    ;;
+esac
 
+cp $i3template $i3src
+
+sed -i "s|{{MOD_KEY}}|set \$mod $modKey|" $i3src
+
+if i3 -c $startupLayout -C > /dev/null; then
+  sed -i \
+    -e "/{{STARTUP_LAYOUT}}/r $startupLayout" \
+    -e "/{{STARTUP_LAYOUT}}/d" $i3src
+else
+  sed -i "/{{STARTUP_LAYOUT}}/d" $i3src
+fi
+
+sed -i \
+  -e "/{{COLOR_SCHEME}}/r $colorScheme" \
+  -e "/{{COLOR_SCHEME}}/d" $i3src
+
+cp $i3src $i3dest
