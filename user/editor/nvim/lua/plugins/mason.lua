2d3b8c480ab420edbe9b9fff87afd21ed9a9d753
Clint Jordan <clintaddison@gmail.com>
2026-07-10 06:48:52 -0600

    auto install language servers

--- user/editor/nvim/lua/plugins/mason.lua
+++ user/editor/nvim/lua/plugins/mason.lua
@@ -1,9 +1,28 @@
 return {
-  "williamboman/mason.nvim",
-  opts = {
-    registries = {
-      "github:mason-org/mason-registry",
-      "github:Crashdummyy/mason-registry",
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      registries = {
+        "github:mason-org/mason-registry",
+        "github:Crashdummyy/mason-registry",
+      },
+    },
+  },
+  {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+    dependencies = { "williamboman/mason.nvim" },
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "basedpyright",
+        "ruff",
+        "bash-language-server",
+        "typescript-language-server",
+        "clangd",
+        "rust-analyzer",
+        "roslyn",
+      },
+      run_on_start = true,
     },
   },
 }
